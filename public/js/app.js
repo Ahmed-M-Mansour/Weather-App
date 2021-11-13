3386dbdf85e382330bd17eaa9a2b3d6db9874270
Ahmed-M-Mansour <ahmedmmansour18@gmail.com>
2021-11-13 09:05:42 +0200

    first version of my App

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+let search = document.querySelector('input') 
+const weatherForm = document.querySelector('form') ; 
+let forcast = document.getElementById('forcast') ; 
+let cityLocation = document.getElementById('location') ; 
+let forError = document.getElementById('error') ; 
+
+cityLocation.textContent = 'Location..'
+
+weatherForm.addEventListener('submit' , e =>{
+    let  location = search.value
+    console.log(location)
+    e.preventDefault() ; 
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then(res =>{
+    res.json().then(data=>{
+            if(data){
+            forcast.textContent = data.forcast ; 
+            cityLocation.textContent = data.location  ; 
+            forError.textContent =  data.error ; 
+        } else{
+            forcast.textContent = data.error ; 
+            console.log(data.error)
+        }
+        
+    })
+   
+    
+}) 
+search.value = ''
+})
+
+
+
---
+++ src/app.js
@@ -0,0 +1,128 @@
+const path = require('path')
+const express = require('express') ; 
+const hbs = require('hbs') 
+const forcast = require('./utils/forcast') ;
+const geoCode = require('./utils/geocode') ; 
+const { send } = require('process');
+// ceate a server 
+const app = express(); 
+// Define paths for express config 
+const publicDirectoryPath = path.join(__dirname , '../public') ; 
+const viewsPath = path.join(__dirname , '../templates/views') ; 
+const partialsPath = path.join(__dirname , '../templates/partials') ; 
+
+
+// To set Up hbs package with express --. to set up handlebars engine and view locations 
+// hbs --> refares to handle bars 
+app.set('view engine', 'hbs');
+app.set('views' , viewsPath) ; 
+hbs.registerPartials(partialsPath)
+
+// set up static directory to serve  
+app.use(express.static(publicDirectoryPath))
+// serve index page 
+app.get('' , (req , res)=>{
+    res.render('index' , {
+        title: "Weather App " , 
+        name: "Ahmed Mansour "
+    }) ; 
+})
+
+// serve About Page 
+app.get('/about' , (req , res) =>{
+    res.render('about' , {
+        title: "About Me  " , 
+        name: "Ahmed Mansour "
+    })
+})
+
+// serve Help page 
+app.get('/help' , (req , res) =>{
+    res.render('help' , {
+        title: "Help Me  " , 
+        name: "Ahmed Mansour "
+    })
+})
+
+// serve products page 
+app.get('/products' , (req , res)=>{
+    if(!req.query.search){
+        return res.send({
+            error: 'you must provide a search term '
+        })
+    }
+    console.log(req.query) 
+    res.send({
+        products: [] 
+    })
+})
+
+// serve weather page 
+app.get('/weather' , (req , res)=>{
+    const address = req.query.address ; 
+    if(!address){
+        return res.send({
+            Erroe: "please enter the Address"
+        })
+    }
+   
+    geoCode(address , (error , {latitude ,longitude , locaton} ={} )=>{
+        if(error){
+            return res.send({error}) ; 
+        }
+        forcast(latitude , longitude , locaton , (error , forcastData) =>{
+            if(error){
+                return res.send({error}) ; 
+            }
+            console.log('Data', forcastData);
+            res.send({
+                forcast : forcastData , 
+                location: locaton ,
+                address : address
+            })
+        } )
+    })
+
+   
+})
+
+
+
+app.get('/help/*' , (req , res ) =>{
+    res.render('404' , {
+        title: "404" , 
+        name: "Ahmed Mansour " , 
+        errorMessage: 'Help Not Found! '
+    })
+})
+
+// if Page not found 
+app.get('*' , (req , res ) =>{
+    res.render('404' , {
+        title: "404" , 
+        name: "Ahmed Mansour " , 
+        errorMessage: 'Page Not Found! '
+    })
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000')
+})
+
+
+
+
+// app.get('' , (req , res )=>{
+//     res.send('Finally run Express App! ')
+// })
+// serve help page 
+// app.get('/help' , (req , res)=>{
+//     app.use(express.static(`${publicDirectoryPath}/help.html`))
+// })
+// // serve about page 
+// app.get('/about' , (req , res )=>{
+//     res.send('<h1> About Page </h1>')
+// })
+
